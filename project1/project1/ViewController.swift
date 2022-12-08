9bc490f860eab4b79e7f576d921fc7287173b7b5
Kiny <89192734912nik@gmail.com>
2022-12-08 20:23:34 +0300

    project1-demofile

---
+++ project1/project1/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by nikita on 07.12.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true 
+       
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try? fm.contentsOfDirectory(atPath: path)
+        
+        for item in items! {
+            if item.hasPrefix("nssl") {
+                // find picture to load
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            let imageName = pictures[indexPath.row]
+            vc.selectedImage = imageName
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
