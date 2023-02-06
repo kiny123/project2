4f1b70817e0f9053dafb55a465591bc2ca6fe9bb
Kiny <89192734912nik@gmail.com>
2023-02-06 22:48:20 +0300

    project12

--- project1/project1/ViewController.swift
+++ project1/project1/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UITableViewController {
     var pictures = [String]()
+    var imageCount = [UserCustomDefults]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Storm Viewer"
-        navigationController?.navigationBar.prefersLargeTitles = true 
-       
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        performSelector(inBackground: #selector(fileManage), with: nil)
+    }
+    
+    @objc func fileManage() {
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try? fm.contentsOfDirectory(atPath: path)
@@ -26,14 +32,22 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
+        
         pictures.sort()
-        print(pictures)
-        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+        tableView.reloadData()
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        let currentCount = UserDefaults.standard.integer(forKey: pictures[indexPath.row])
+        cell.detailTextLabel?.text = String(currentCount)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
     }
@@ -41,6 +55,12 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
+            
+            guard let selectedImage = vc.selectedImage else { return }
+            let curentImageCount = UserDefaults.standard.integer(forKey: selectedImage)
+            let currentCount = curentImageCount + 1
+            UserDefaults.standard.set(currentCount, forKey: selectedImage)
+            
             vc.selectedPictureNumber = indexPath.row + 1
             vc.totalPictures = pictures.count
             navigationController?.pushViewController(vc, animated: true)
